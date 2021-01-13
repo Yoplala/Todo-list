dc27101fd299d867b5e89dced28420db20deb3a8
Yoplala <yoplala@pataraphe.net>
2021-01-13 13:05:45 +0100

    Correction #5
    Supression d'un affichage console inutile

--- js/controller.js
+++ js/controller.js
@@ -156,12 +156,6 @@
 			items = data;
 		});
 
-		items.forEach(function(item) {
-			if (item.id === id) {
-				console.log("Element with ID: " + id + " has been removed.");
-			}
-		});
-
 		self.model.remove(id, function () {
 			self.view.render('removeItem', id);
 		});

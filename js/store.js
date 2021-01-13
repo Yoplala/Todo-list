e407270ce845cbac556ba349bce200d71bf635f2
Yoplala <yoplala@pataraphe.net>
2021-01-13 12:58:55 +0100

    Correction #2
    Génération d'ID optimisée.
    - Plus de génération d'ID en dehors du if/else.
    - Plus de risque de doublon avec la méthode Date.now().

--- js/store.js
+++ js/store.js
@@ -80,14 +80,6 @@
 
 		callback = callback || function () {};
 
-		// Generate an ID
-	    var newId = ""; 
-	    var charset = "0123456789";
-
-        for (var i = 0; i < 6; i++) {
-     		newId += charset.charAt(Math.floor(Math.random() * charset.length));
-		}
-
 		// If an ID was actually given, find the item and update each property
 		if (id) {
 			for (var i = 0; i < todos.length; i++) {
@@ -103,10 +95,9 @@
 			callback.call(this, todos);
 		} else {
 
-    		// Assign an ID
-			updateData.id = parseInt(newId);
+    		// Generate and assign an ID
+			updateData.id = Date.now();
     
-
 			todos.push(updateData);
 			localStorage[this._dbName] = JSON.stringify(data);
 			callback.call(this, [updateData]);

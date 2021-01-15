fcfc401aeb77ed94d1a9610e11a10c7e980678c6
Yoplala <yoplala@pataraphe.net>
2021-01-15 17:50:01 +0100

    Test #8

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -199,6 +199,12 @@ describe('controller', function () {
 	describe('new todo', function () {
 		it('should add a new todo to the model', function () {
 			// TODO: write test
+			setUpModel([]);
+
+			subject.setView('');
+			view.trigger('newTodo', 'a new todo');
+
+			expect(model.create).toHaveBeenCalledWith('a new todo', jasmine.any(Function));
 		});
 
 		it('should add a new todo to the view', function () {
